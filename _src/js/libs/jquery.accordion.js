d4ed8c4704a8383d8ef8138933c1ff4e386afadc
unknown <al.tulin@gmail.com>
2021-08-15 20:56:50 +0800

    debug accordeon

--- _src/js/libs/jquery.accordion.js
+++ _src/js/libs/jquery.accordion.js
@@ -1,180 +1,302 @@
-!(function (t, n, e, a) {
-  const i = "accordion";
-  const o = {
-    transitionSpeed: 300,
-    transitionEasing: "ease",
-    controlElement: "[data-control]",
-    contentElement: "[data-content]",
-    groupElement: "[data-accordion-group]",
-    singleOpen: !0,
-  };
-  function s(n, e) {
-    (this.element = n),
-      (this.options = t.extend({}, o, e)),
-      (this._defaults = o),
-      (this._name = i),
+/*!
+ * jQuery Accordion 0.0.1
+ * (c) 2014 Victor Fernandez <victor@vctrfrnndz.com>
+ * MIT Licensed.
+ */
+
+;(function ( $, window, document, undefined ) {
+
+  var pluginName = 'accordion',
+      defaults = {
+          transitionSpeed: 300,
+          transitionEasing: 'ease',
+          controlElement: '[data-control]',
+          contentElement: '[data-content]',
+          groupElement: '[data-accordion-group]',
+          singleOpen: true
+      };
+
+  function Accordion(element, options) {
+      this.element = element;
+      this.options = $.extend({}, defaults, options);
+      this._defaults = defaults;
+      this._name = pluginName;
       this.init();
   }
-  (s.prototype.init = function () {
-    let a;
-    let i;
-    let o;
-    let s;
-    const r = this.options;
-    const c = t(this.element);
-    const d = c.find(`> ${r.controlElement}`);
-    const h = c.find(`> ${r.contentElement}`);
-    const l = c.parents("[data-accordion]").length > 0;
-    const f = { "max-height": 0, overflow: "hidden" };
-    const m = (function () {
-      const t = (e.body || e.documentElement).style;
-      let n = "transition";
-      if (typeof t[n] === "string") return !0;
-      const a = ["Moz", "webkit", "Webkit", "Khtml", "O", "ms"];
-      n = "Transition";
-      for (let i = 0; i < a.length; i++)
-        if (typeof t[a[i] + n] === "string") return !0;
-      return !1;
-    })();
-    function g(t, n) {
-      n
-        ? h.css({ "-webkit-transition": "", transition: "" })
-        : h.css({
-            "-webkit-transition": `max-height ${r.transitionSpeed}ms ${r.transitionEasing}`,
-            transition: `max-height ${r.transitionSpeed}ms ${r.transitionEasing}`,
-          });
-    }
-    function u(n) {
-      let e = 0;
-      n.children().each(function () {
-        e += t(this).outerHeight(!0);
-      }),
-        n.data("oHeight", e);
-    }
-    function p(n, e, a, i) {
-      let o;
-      const s = n.filter(".open").find("> [data-content]");
-      let c = s.find("[data-accordion].open > [data-content]");
-      r.singleOpen ||
-        (c = c.not(
-          e.siblings("[data-accordion].open").find("> [data-content]")
-        )),
-        (o = s.add(c)),
-        n.hasClass("open") &&
-          o.each(function () {
-            const n = t(this).data("oHeight");
-            switch (i) {
-              case "+":
-                t(this).data("oHeight", n + a);
-                break;
-              case "-":
-                t(this).data("oHeight", n - a);
-                break;
-              default:
-                throw "updateParentHeight method needs an operation";
-            }
-            t(this).css("max-height", t(this).data("oHeight"));
-          });
-    }
-    function v(t) {
-      if (t.hasClass("open")) {
-        const n = t.find("> [data-content]");
-        const e = n.find("[data-accordion].open > [data-content]");
-        const a = n.add(e);
-        u(a), a.css("max-height", a.data("oHeight"));
+
+  Accordion.prototype.init = function () {
+      var self = this,
+          opts = self.options;
+
+      var $accordion = $(self.element),
+          $controls = $accordion.find('> ' + opts.controlElement),
+          $content =  $accordion.find('> ' + opts.contentElement);
+
+      var accordionParentsQty = $accordion.parents('[data-accordion]').length,
+          accordionHasParent = accordionParentsQty > 0;
+
+      var closedCSS = { 'max-height': 0, 'overflow': 'hidden' };
+
+      var CSStransitions = supportsTransitions();
+
+      function debounce(func, threshold, execAsap) {
+          var timeout;
+
+          return function debounced() {
+              var obj = this,
+                  args = arguments;
+
+              function delayed() {
+                  if (!execAsap) func.apply(obj, args);
+                  timeout = null;
+              };
+
+              if (timeout) clearTimeout(timeout);
+              else if (execAsap) func.apply(obj, args);
+
+              timeout = setTimeout(delayed, threshold || 100);
+          };
       }
-    }
-    function E(t, n) {
-      if ((t.trigger("accordion.close"), m)) {
-        if (l) p(t.parents("[data-accordion]"), t, n.data("oHeight"), "-");
-        n.css(f), t.removeClass("open");
-      } else
-        n.css("max-height", n.data("oHeight")),
-          n.animate(f, r.transitionSpeed),
-          t.removeClass("open");
-    }
-    function H(t, e) {
-      if ((t.trigger("accordion.open"), m)) {
-        if ((g(), l))
-          p(t.parents("[data-accordion]"), t, e.data("oHeight"), "+");
-        (a = function () {
-          e.css("max-height", e.data("oHeight"));
-        }),
-          n.requestAnimationFrame
-            ? requestAnimationFrame(a)
-            : n.webkitRequestAnimationFrame
-            ? webkitRequestAnimationFrame(a)
-            : n.mozRequestAnimationFrame
-            ? mozRequestAnimationFrame(a)
-            : setTimeout(a, 1e3 / 60),
-          t.addClass("open");
-      } else
-        e.animate(
-          { "max-height": e.data("oHeight") },
-          r.transitionSpeed,
-          function () {
-            e.css({ "max-height": "none" });
+
+      function supportsTransitions() {
+          var b = document.body || document.documentElement,
+              s = b.style,
+              p = 'transition';
+
+          if (typeof s[p] == 'string') {
+              return true;
           }
-        ),
-          t.addClass("open");
-      let a;
-    }
-    function x() {
-      const n = !!r.singleOpen && c.parents(r.groupElement).length > 0;
-      u(h),
-        n &&
-          (function (n) {
-            n.closest(r.groupElement);
-            const e = n.siblings("[data-accordion]").filter(".open");
-            const a = e.find("[data-accordion]").filter(".open");
-            const i = e.add(a);
-            i.each(function () {
-              const n = t(this);
-              const e = n.find(r.contentElement);
-              E(n, e);
-            }),
-              i.removeClass("open");
-          })(c),
-        c.hasClass("open") ? E(c, h) : H(c, h);
-    }
-    h.each(function () {
-      const n = t(this);
-      n.css("max-height") != 0 &&
-        (n.closest("[data-accordion]").hasClass("open")
-          ? (g(), u(n), n.css("max-height", n.data("oHeight")))
-          : n.css({ "max-height": 0, overflow: "hidden" }));
-    }),
-      c.attr("data-accordion") ||
-        (c.attr("data-accordion", ""),
-        c.find(r.controlElement).attr("data-control", ""),
-        c.find(r.contentElement).attr("data-content", "")),
-      d.on("click", x),
-      d.on("accordion.toggle", function () {
-        if (r.singleOpen && d.length > 1) return !1;
-        x();
-      }),
-      d.on("accordion.refresh", function () {
-        v(c);
-      }),
-      t(n).on(
-        "resize",
-        ((a = function () {
-          v(c);
-        }),
-        function () {
-          const t = this;
-          const n = arguments;
-          s ? clearTimeout(s) : o && a.apply(t, n),
-            (s = setTimeout(function () {
-              o || a.apply(t, n), (s = null);
-            }, i || 100));
-        })
-      );
-  }),
-    (t.fn[i] = function (n) {
+
+          var v = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
+
+          p = 'Transition';
+
+          for (var i=0; i<v.length; i++) {
+              if (typeof s[v[i] + p] == 'string') {
+                  return true;
+              }
+          }
+
+          return false;
+      }
+
+      function requestAnimFrame(cb) {
+          if(window.requestAnimationFrame){
+              requestAnimationFrame(cb);
+          } else if (window.webkitRequestAnimationFrame) {
+              webkitRequestAnimationFrame(cb);
+          } else if (window.mozRequestAnimationFrame) {
+              mozRequestAnimationFrame(cb);
+          } else {
+              setTimeout(cb, 1000 / 60);
+          }
+      }
+
+      function toggleTransition($el, remove) {
+          if(!remove) {
+              $content.css({
+                  '-webkit-transition': 'max-height ' + opts.transitionSpeed + 'ms ' + opts.transitionEasing,
+                  'transition': 'max-height ' + opts.transitionSpeed + 'ms ' + opts.transitionEasing
+              });
+          } else {
+              $content.css({
+                  '-webkit-transition': '',
+                  'transition': ''
+              });
+          }
+      }
+
+      function calculateHeight($el) {
+          var height = 0;
+
+          $el.children().each(function() {
+              height = height + $(this).outerHeight(true);
+          });
+
+          $el.data('oHeight', height);
+      }
+
+      function updateParentHeight($parentAccordion, $currentAccordion, qty, operation) {
+          var $content = $parentAccordion.filter('.open').find('> [data-content]'),
+              $childs = $content.find('[data-accordion].open > [data-content]'),
+              $matched;
+
+          if(!opts.singleOpen) {
+              $childs = $childs.not($currentAccordion.siblings('[data-accordion].open').find('> [data-content]'));
+          }
+
+          $matched = $content.add($childs);
+
+          if($parentAccordion.hasClass('open')) {
+              $matched.each(function() {
+                  var currentHeight = $(this).data('oHeight');
+
+                  switch (operation) {
+                      case '+':
+                          $(this).data('oHeight', currentHeight + qty);
+                          break;
+                      case '-':
+                          $(this).data('oHeight', currentHeight - qty);
+                          break;
+                      default:
+                          throw 'updateParentHeight method needs an operation';
+                  }
+
+                  $(this).css('max-height', $(this).data('oHeight'));
+              });
+          }
+      }
+
+      function refreshHeight($accordion) {
+          if($accordion.hasClass('open')) {
+              var $content = $accordion.find('> [data-content]'),
+                  $childs = $content.find('[data-accordion].open > [data-content]'),
+                  $matched = $content.add($childs);
+
+              calculateHeight($matched);
+
+              $matched.css('max-height', $matched.data('oHeight'));
+          }
+      }
+
+      function closeAccordion($accordion, $content) {
+          $accordion.trigger('accordion.close');
+
+          if(CSStransitions) {
+              if(accordionHasParent) {
+                  var $parentAccordions = $accordion.parents('[data-accordion]');
+
+                  updateParentHeight($parentAccordions, $accordion, $content.data('oHeight'), '-');
+              }
+
+              $content.css(closedCSS);
+
+              $accordion.removeClass('open');
+          } else {
+              $content.css('max-height', $content.data('oHeight'));
+
+              $content.animate(closedCSS, opts.transitionSpeed);
+
+              $accordion.removeClass('open');
+          }
+      }
+
+      function openAccordion($accordion, $content) {
+          $accordion.trigger('accordion.open');
+          if(CSStransitions) {
+              toggleTransition($content);
+
+              if(accordionHasParent) {
+                  var $parentAccordions = $accordion.parents('[data-accordion]');
+
+                  updateParentHeight($parentAccordions, $accordion, $content.data('oHeight'), '+');
+              }
+
+              requestAnimFrame(function() {
+                  $content.css('max-height', $content.data('oHeight'));
+              });
+
+              $accordion.addClass('open');
+          } else {
+              $content.animate({
+                  'max-height': $content.data('oHeight')
+              }, opts.transitionSpeed, function() {
+                  $content.css({'max-height': 'none'});
+              });
+
+              $accordion.addClass('open');
+          }
+      }
+
+      function closeSiblingAccordions($accordion) {
+          var $accordionGroup = $accordion.closest(opts.groupElement);
+
+          var $siblings = $accordion.siblings('[data-accordion]').filter('.open'),
+              $siblingsChildren = $siblings.find('[data-accordion]').filter('.open');
+
+          var $otherAccordions = $siblings.add($siblingsChildren);
+
+          $otherAccordions.each(function() {
+              var $accordion = $(this),
+                  $content = $accordion.find(opts.contentElement);
+
+              closeAccordion($accordion, $content);
+          });
+
+          $otherAccordions.removeClass('open');
+      }
+
+      function toggleAccordion() {
+          var isAccordionGroup = (opts.singleOpen) ? $accordion.parents(opts.groupElement).length > 0 : false;
+
+          calculateHeight($content);
+
+          if(isAccordionGroup) {
+              closeSiblingAccordions($accordion);
+          }
+
+          if($accordion.hasClass('open')) {
+              closeAccordion($accordion, $content);
+          } else {
+              openAccordion($accordion, $content);
+          }
+      }
+
+      function addEventListeners() {
+          $controls.on('click', toggleAccordion);
+
+          $controls.on('accordion.toggle', function() {
+              if(opts.singleOpen && $controls.length > 1) {
+                  return false;
+              }
+
+              toggleAccordion();
+          });
+
+          $controls.on('accordion.refresh', function() {
+              refreshHeight($accordion);
+          });
+
+          $(window).on('resize', debounce(function() {
+              refreshHeight($accordion);
+          }));
+      }
+
+      function setup() {
+          $content.each(function() {
+              var $curr = $(this);
+
+              if($curr.css('max-height') != 0) {
+                  if(!$curr.closest('[data-accordion]').hasClass('open')) {
+                      $curr.css({ 'max-height': 0, 'overflow': 'hidden' });
+                  } else {
+                      toggleTransition($curr);
+                      calculateHeight($curr);
+
+                      $curr.css('max-height', $curr.data('oHeight'));
+                  }
+              }
+          });
+
+
+          if(!$accordion.attr('data-accordion')) {
+              $accordion.attr('data-accordion', '');
+              $accordion.find(opts.controlElement).attr('data-control', '');
+              $accordion.find(opts.contentElement).attr('data-content', '');
+          }
+      }
+
+      setup();
+      addEventListeners();
+  };
+
+  $.fn[pluginName] = function ( options ) {
       return this.each(function () {
-        t.data(this, `plugin_${i}`) ||
-          t.data(this, `plugin_${i}`, new s(this, n));
+          if (!$.data(this, 'plugin_' + pluginName)) {
+              $.data(this, 'plugin_' + pluginName,
+              new Accordion( this, options ));
+          }
       });
-    });
-})(jQuery, window, document);
+  }
+
+})( jQuery, window, document );
